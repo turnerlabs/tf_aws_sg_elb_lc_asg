1004cda4cd0bb9dd4678aff8f94c7b5368c6a841
Michael Smith <mike.smith@turner.com>
2016-04-26 08:06:33 -0400

    cleanup

--- example/example.tf
+++ example/example.tf
@@ -1,5 +1,5 @@
 module "my_autoscaling_group" {
-  source = "https://github.com/smithatlanta/tf_aws_sg_elb_lc_asg"
+  source = "github.com/smithatlanta/tf_aws_sg_elb_lc_asg"
 
   sg_elb_name = "${var.sg_elb_name}"
 
@@ -17,15 +17,9 @@ module "my_autoscaling_group" {
 
   key_name = "${var.key_name}"
 
-  security_group = "${var.security_group_id}"
-
-  user_data = "${var.user_data_file}"
-
   asg_name = "${var.asg_name}"
   asg_number_of_instances = "${var.asg_number_of_instances}"
   asg_minimum_number_of_instances = "${var.asg_minimum_number_of_instances}"
-
-  load_balancer_names = "${var.elb_names}"
 
   health_check_type = "${var.health_check_type}"
 
--- variables.tf
+++ variables.tf
@@ -28,19 +28,12 @@ variable "ami_id" {
   description = "The AMI to use with the launch configuration"
 }
 variable "instance_type" {}
-variable "iam_instance_profile" {
-  description = "The IAM role the launched instance will use"
-}
 variable "key_name" {
   description = "The SSH public key name (in EC2 key-pairs) to be injected into instances"
 }
 variable "security_group" {
   description = "ID of SG the launched instance will use"
 }
-variable "user_data" {
-  description = "The path to a file with user_data for the instances"
-}
-
 #
 # Auto-Scaling Group
 #
